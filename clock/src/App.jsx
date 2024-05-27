5a206140248784ea9dd7fbfa8417143060daf335
ToonvanBerkel <143796002+ToonvanBerkel@users.noreply.github.com>
2024-05-27 14:52:11 +0200

    push

---
+++ clock/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Clock from './components/Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Current Time</h1>
+      <Clock />
+    </div>
+  );
+}
+
+export default App;
---
+++ clock/src/components/Clock.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+const Clock = () => {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div>
+      {currentTime.toLocaleTimeString()}
+    </div>
+  );
+};
+
+export default Clock;
